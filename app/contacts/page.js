b5031201c87589bee82d6547fa641377b2ded4f9
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-12-06 15:24:22 +0200

    added fetch data

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,11 +1,35 @@
 const { default: Heading } = require("@/components/Heading");
 
+const getContacts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: {
+      revalidate: 60,
+    },
+  });
+
+  return res.json();
+};
+
 export const metadata = {
   title: "Contacts",
 };
 
-const Contacts = () => {
-  return <Heading text="Contacts list:" />;
+const Contacts = async () => {
+  const contacts = await getContacts();
+
+  return (
+    <>
+      <Heading text="Contacts list:" />
+      <ul>
+        {contacts &&
+          contacts.map(({ id, name, email }) => (
+            <li key={id}>
+              <strong>{name}</strong> {email}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 
 export default Contacts;
